e543abd402adea7dc3bdff72d47e4c4630300a78
Ebrahimsamad <ebrahim7asn@gmail.com>
2024-09-26 18:49:39 +0300

    BUGFIX:Fix Email Verify

--- controllers/user.js
+++ controllers/user.js
@@ -107,7 +107,7 @@ exports.signup = async (req, res, next) => {
             </div>
             <div class="content">
               <p>To verify your email, please use the following button:</p>
-              <a href="https://chat-ebrahim-samad.vercel.app/?email=${user.email}">verifie your email</a>
+              <a href="https://chat-ebrahim-samad.vercel.app/verify-email?email=${user.email}">verifie your email</a>
             </div>
           </div>
         </body>
